e24195afd1feb0ccb3b090c811b5824b209d09be
Maxim Kolotovkin Cactus Soft <maxim.kolotovkin@cactussoft.biz>
2023-07-04 22:02:27 +0400

    Rate us btn

--- Assets/Scripts/WelcomeSceneControl.cs
+++ Assets/Scripts/WelcomeSceneControl.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WelcomeSceneControl : MonoBehaviour {
+    public void RateUsBtnClick() {
+        try {
+            const string Url = "https://play.google.com/store/apps/details?id=com.K.O.MaxGames.KillingSaw";
+            Application.OpenURL(Url.Trim());
+        } catch {
+            Debug.Log("Error in - Rate Us Btn Click");
+        }
+    }
+
     private void MinimumMoneySet() {
         int totalMoney = PlayerPrefs.GetInt("TOTAL_MONEY_KEY", 0);
         if (totalMoney < 85) PlayerPrefs.SetInt("TOTAL_MONEY_KEY", 85);
